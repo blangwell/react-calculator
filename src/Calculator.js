cb8abf346ec1695862941f8e92c08dc0da097e94
blangwell <b.langwell@outlook.com>
2020-08-26 16:59:36 -0700

    [calculator] Add Calculator component

---
+++ src/Calculator.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Calculator extends Component {
+    constructor (props) {
+        super()
+        this.state = {
+            num1: 0,
+            num2: 0,
+            result: 0
+        }
+    }
+
+    render() {
+        return(
+        <div className="container">
+            <h1>Add with React!</h1>
+            <div className="add">
+                <input type="text" />
+                <span>+</span>
+                <input type="text" />
+                <button>=</button>
+                <h3>Addition results go here!</h3>
+            </div>
+        </div>
+        )
+    }
+}
