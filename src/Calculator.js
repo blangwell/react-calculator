a0806f947052a8b570077128d69599503cef5924
blangwell <b.langwell@outlook.com>
2020-08-26 17:11:33 -0700

    [calc] Add to Calculator component and import into index.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,15 +9,27 @@ class Calculator extends Component {
             result: 0
         }
     }
+    // [num] in setState allows us to use key values dynamically!
+    setNum = (e, num) => {
+        this.setState({ [num]: e.target.value })
+    }
 
     render() {
         return(
         <div className="container">
             <h1>Add with React!</h1>
             <div className="add">
-                <input type="text" />
+                <input type="number"
+                name="num1"
+                placeholder="Enter your first number"
+                value={this.state.num1}
+                onChange={e => this.setNum(e, 'num1')} />
                 <span>+</span>
-                <input type="text" />
+                <input type="number"
+                name="num2"
+                placeholder="Enter your second number"
+                value={this.state.num2}
+                onChange={e => this.setNum(e, 'num2')} />
                 <button>=</button>
                 <h3>Addition results go here!</h3>
             </div>
@@ -25,3 +37,5 @@ class Calculator extends Component {
         )
     }
 }
+
+export default Calculator;
